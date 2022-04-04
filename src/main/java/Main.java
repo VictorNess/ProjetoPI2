ccff55b6ef141eff79f6f18d2899b8ebd23b399e
michael.santos <michael.santos@TI02.HMGINTS.LOCAL>
2022-04-04 07:42:27 -0300

    "Equipe" > "Criado Classe e metododos acessores e passado para o diego dat continuidade"

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author michael.santos
+ */
+public class Main {
+
+
+        
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ src/main/java/Produto.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author michael.santos
+ */
+public class Produto {
+    private String cor;
+    private String modelo;
+    private int valor;
+    private int tamanho;
+
+    /**
+     * @return the cor
+     */
+    public String getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the valor
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the tamanho
+     */
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    /**
+     * @param tamanho the tamanho to set
+     */
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+}
+
+
