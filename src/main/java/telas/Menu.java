e35b4ccefcf0bc417947add1bf8d108cda648d18
Camilly <camilly.bsantos2@senacsp.edu.br>
2022-04-15 20:10:51 -0300

    Menu

--- src/main/java/telas/Menu.java
+++ src/main/java/telas/Menu.java
@@ -6,12 +6,12 @@ package telas;
 
 /**
  *
- * @author Victo
+ * @author Camilly
  */
 public class Menu extends javax.swing.JFrame {
 
     /**
-     * Creates new form Menu
+     * Creates new form telaVenda
      */
     public Menu() {
         initComponents();
@@ -26,37 +26,61 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        mnuInternoConsCliente = new javax.swing.JDesktopPane();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        mnuConsCliente = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        mnuConsProduto = new javax.swing.JMenu();
+        mnuEfetCompra = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 3));
-
-        jButton1.setText("inciar");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(144, 144, 144)
-                .addComponent(jButton1)
-                .addContainerGap(163, Short.MAX_VALUE))
+        javax.swing.GroupLayout mnuInternoConsClienteLayout = new javax.swing.GroupLayout(mnuInternoConsCliente);
+        mnuInternoConsCliente.setLayout(mnuInternoConsClienteLayout);
+        mnuInternoConsClienteLayout.setHorizontalGroup(
+            mnuInternoConsClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(159, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(72, 72, 72))
+        mnuInternoConsClienteLayout.setVerticalGroup(
+            mnuInternoConsClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 580, Short.MAX_VALUE)
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 370, 260));
+        getContentPane().add(mnuInternoConsCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 580));
+
+        mnuConsCliente.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        mnuConsCliente.setText("Consultar Cliente");
+
+        jMenuItem1.setText("Consultar/Cadastrar");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        mnuConsCliente.add(jMenuItem1);
+
+        jMenuBar1.add(mnuConsCliente);
+
+        mnuConsProduto.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        mnuConsProduto.setText("Consultar Produto");
+        jMenuBar1.add(mnuConsProduto);
+
+        mnuEfetCompra.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        mnuEfetCompra.setText("Efetuar  compra");
+        jMenuBar1.add(mnuEfetCompra);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+         mnuInternoConsCliente consultarCliente = new mnuInternoConsCliente();
+        consultarCliente.setVisible(true);
+        
+        this.mnuInternoConsCliente.add(consultarCliente);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -84,6 +108,7 @@ public class Menu extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -94,7 +119,11 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenu mnuConsCliente;
+    private javax.swing.JMenu mnuConsProduto;
+    private javax.swing.JMenu mnuEfetCompra;
+    private javax.swing.JDesktopPane mnuInternoConsCliente;
     // End of variables declaration//GEN-END:variables
 }
