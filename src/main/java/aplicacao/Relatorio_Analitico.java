6fbc94ca4f74eb0856cc599d71ed476a8d77f7b2
Maickk0 <94754415+Maickk0@users.noreply.github.com>
2022-05-03 09:13:28 -0300

    Update Relatorio_Analitico.java

--- src/main/java/aplicacao/Relatorio_Analitico.java
+++ src/main/java/aplicacao/Relatorio_Analitico.java
@@ -7,33 +7,4 @@ package aplicacao;
 public class Relatorio_Analitico {
    
 
-        float consulVendas;
-        String ConsultaNome;
-        String ConsultaData;
-
-        public float getConsulVendas() {
-            return consulVendas;
-        }
-
-        public void setConsulVendas(float consulVendas) {
-            this.consulVendas = consulVendas;
-        }
-
-        public String getConsultaNome() {
-            return ConsultaNome;
-        }
-
-        public void setConsultaNome(String ConsultaNome) {
-            this.ConsultaNome = ConsultaNome;
-        }
-
-        public String getConsultaData() {
-            return ConsultaData;
-        }
-
-        public void setConsultaData(String ConsultaData) {
-            this.ConsultaData = ConsultaData;
-        }
-
-    }
-
+}
