014bd2465db61e9c5ebd99b15ff9ddbff282889d
Maickk0 <94754415+Maickk0@users.noreply.github.com>
2022-05-03 09:28:36 -0300

    Update Relatorio_Sintetico.java

--- src/main/java/aplicacao/Relatorio_Sintetico.java
+++ src/main/java/aplicacao/Relatorio_Sintetico.java
@@ -6,34 +6,7 @@ package aplicacao;
  */
 public class Relatorio_Sintetico {
      
+     Date dataFinal;
+          Date dataInicio;
 
-        float consulVendas;
-        String ConsultaNome;
-        String ConsultaData;
-
-        public float getConsulVendas() {
-            return consulVendas;
-        }
-
-        public void setConsulVendas(float consulVendas) {
-            this.consulVendas = consulVendas;
-        }
-
-        public String getConsultaNome() {
-            return ConsultaNome;
-        }
-
-        public void setConsultaNome(String ConsultaNome) {
-            this.ConsultaNome = ConsultaNome;
-        }
-
-        public String getConsultaData() {
-            return ConsultaData;
-        }
-
-        public void setConsultaData(String ConsultaData) {
-            this.ConsultaData = ConsultaData;
-        }
-
-    }
-
+}
